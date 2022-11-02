352a50926a1a65ac2a811fef608dd7a42edbaa4d
LostButton <71197188+LostButton@users.noreply.github.com>
2022-11-02 18:01:25 -0400

    First gallery view commit

---
+++ src/gallery.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+document.getElementById("app").innerHTML = `
+<h1>Hello User!</h1>
+<div>
+There's never been a time where people had to cope with more images. At the same time, most companies take advantage of our desire to share what we create, charging for storage.
+
+My Photohub is designed to leverage the open web and make it easy for anyone to take a bunch of photos and turn them into a web page they can share with the world. You don't need to be a programmer to take advantage of the web or GitHub in order to share your photos.
+</div>
+`;
+
+
+
+
