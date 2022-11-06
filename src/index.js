7ad96ed94dad9d3982d7b6650c341eba4ccdc88a
LostButton <71197188+LostButton@users.noreply.github.com>
2022-11-06 18:17:56 -0500

    fixing pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "@fontsource/poppins";
 import "photoswipe/dist/photoswipe.css"
 import PhotoSwipe from 'photoswipe/dist/photoswipe.esm';
+import "../css/gallery.css"
 
 import Lightbox from 'photoswipe/dist/photoswipe-lightbox.esm';
 const lightbox = new Lightbox({
