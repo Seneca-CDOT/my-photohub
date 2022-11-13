33c7edb581b6a3d134dc860bf90fef7ea779f5e2
batunpc <batuhnpc@gmail.com>
2022-11-13 03:29:20 -0500

    UI: Ability to preview the uploaded images

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
+import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-
-import { Button, Container, Form, Modal, Navbar } from "react-bootstrap";
+import { FileUploader } from "react-drag-drop-files";
+import {
+  Button,
+  Container,
+  Form,
+  Modal,
+  Navbar,
+  OverlayTrigger,
+  Tooltip,
+} from "react-bootstrap";
 
 function App() {
   const [token, setToken] = React.useState(null);
   const [repository, setRepository] = React.useState(null);
   const [show, setShow] = React.useState(false);
+  const [image, setImage] = React.useState(null);
 
   return (
     <>
@@ -17,7 +27,10 @@ function App() {
           <Navbar.Toggle />
           <Navbar.Collapse className="justify-content-end">
             <Navbar.Text>
-              {repository ? `Using repository ${repository}` : `Not signed in `}
+              {repository
+                ? `Using repository ${repository} `
+                : `Not signed in `}
+              &nbsp;&nbsp;
               <Button
                 variant="outline-success"
                 onClick={(e) => {
@@ -33,6 +46,49 @@ function App() {
       </Navbar>
 
       <div className="App">
+        <header className="preview">
+          <img src={logo} className="App-logo" id="img-preview" alt="logo" />
+          <p>Drag and Drop or Click to Upload Images</p>
+          <FileUploader
+            name="file"
+            types={["jpg", "jpeg", "png", "bmp", "gif"]}
+            multiple={false}
+            classes="Drop-zone"
+            handleChange={(file) => {
+              setImage(file);
+              const reader = new FileReader();
+              reader.onload = (e) => {
+                document.getElementById("img-preview").src = e.target.result;
+              };
+              reader.readAsDataURL(file);
+              document.getElementById("img-preview").style.display = "block";
+              document.getElementById("img-preview").title = file.name;
+            }}
+            label={"Upload or drop a file right here"}
+          />
+          <OverlayTrigger
+            overlay={
+              <Tooltip id="tooltip-disabled">
+                {!image
+                  ? "Select Image to Upload"
+                  : !(token && repository)
+                  ? "Add Token and Repository"
+                  : "Click to Upload"}
+              </Tooltip>
+            }
+            placement="bottom"
+          >
+            <span className="d-inline-block">
+              <Button
+                variant="success"
+                className="mt-3"
+                disabled={!(token && image && repository)}
+              >
+                Upload
+              </Button>
+            </span>
+          </OverlayTrigger>
+        </header>
         <Modal
           show={show}
           onHide={() => {
