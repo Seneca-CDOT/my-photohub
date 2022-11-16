108627576dc4ee6dfc48d149b2bab2afa6e7ad8a
Taimoor Dawami <fahimraza492@gmail.com>
2022-11-15 22:09:51 -0500

    refactor: use str.test() instead of validator's .match()

--- worker/functions/validate-input.js
+++ worker/functions/validate-input.js
@@ -1,12 +1,7 @@
-import validator from "validator";
-
 const isValidInput = (token, repoName) => {
-  const matchesRepoRegex = validator.matches(
-    repoName,
-    "^[A-Z-a-z-0-9\-\_\.]{0,100}$"
-  );
-  const matchesPATRegex = validator.matches(token, "^ghp_[A-Za-z-0-9]+$");
-  return matchesPATRegex && matchesRepoRegex;
-};
+  const isValidPAT = /^ghp_[A-Za-z-0-9]+$/;
+  const isValidRepo = /^[A-Z-a-z-0-9\-\_\.]{0,100}$/;
 
+  return isValidPAT.test(token) && isValidRepo.test(repoName);
+};
 export default isValidInput;
