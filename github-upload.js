4106bec48fc21a773445c01ad74fc5891272cf1d
srpurba <srpurba@myseneca.ca>
2022-11-16 15:44:47 -0500

    remove unused code

--- github-upload.js
+++ github-upload.js
@@ -1,27 +1,25 @@
-import { Octokit, App } from "https://cdn.skypack.dev/octokit?dts";
+import { Octokit } from "https://cdn.skypack.dev/octokit?dts";
 
-const UPLOAD_DIRECTORY = 'raw'
+const UPLOAD_DIRECTORY = "raw";
 
 function getBase64(file) {
-    return new Promise((resolve, reject) => {
-        const reader = new FileReader();
-        reader.readAsDataURL(file);
-        reader.onload = () => resolve(reader.result.substring(reader.result.indexOf(",") + 1));
-        reader.onerror = error => reject(error);
-    });
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () =>
+      resolve(reader.result.substring(reader.result.indexOf(",") + 1));
+    reader.onerror = (error) => reject(error);
+  });
 }
 
 export async function githubUpload(token, repository, file) {
-    const octokit = new Octokit({ auth: token })
-    const [owner, repo] = repository.split('/')
-    return await octokit.request('PUT /repos/{owner}/{repo}/contents/{path}', {
-        owner,
-        repo,
-        path: `${UPLOAD_DIRECTORY}/${file.name}`,
-        message: `Upload File - ${file.name}`,
-        content: await getBase64(file)
-    })
+  const octokit = new Octokit({ auth: token });
+  const [owner, repo] = repository.split("/");
+  return await octokit.request("PUT /repos/{owner}/{repo}/contents/{path}", {
+    owner,
+    repo,
+    path: `${UPLOAD_DIRECTORY}/${file.name}`,
+    message: `Upload File - ${file.name}`,
+    content: await getBase64(file),
+  });
 }
-
-window.githubUpload = githubUpload
-window.getBase64 = getBase64
