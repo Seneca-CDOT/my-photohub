fc22bb4364780cf0d521670538f50f05738cd0e6
Taimoor Dawami <fahimraza492@gmail.com>
2022-11-15 23:14:49 -0500

    Remove unnecessary escape characters from regex

--- worker/functions/validate-input.js
+++ worker/functions/validate-input.js
@@ -1,6 +1,6 @@
 const isValidInput = (token, repoName) => {
   const isValidPAT = /^ghp_[A-Za-z-0-9]+$/;
-  const isValidRepo = /^[A-Z-a-z-0-9\-\_\.]{0,100}$/;
+  const isValidRepo = /^[A-Z-a-z-0-9\-_.]{0,100}$/;
 
   return isValidPAT.test(token) && isValidRepo.test(repoName);
 };
