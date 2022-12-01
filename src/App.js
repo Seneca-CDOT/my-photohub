bf1b043a626b94e8bcbec0688358461892476247
rudychung <61764259+rudychung@users.noreply.github.com>
2022-12-01 18:47:25 -0500

    Add project creation form UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from "react";
-import NavigationBar from "./NavigationBar";
-import AuthDialog from "./AuthDialog";
-import UploadInterface from "./UploadInterface";
+import React from 'react';
+import Stack from '@mui/material/Stack';
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
+
+import NavigationBar from './NavigationBar';
+import AuthDialog from './AuthDialog';
+import UploadInterface from './UploadInterface';
+import ProjectForm from './ProjectForm';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  color: theme.palette.text.secondary,
+  minWidth: '50%',
+}));
 
 function App() {
   const [token, setToken] = React.useState(null);
@@ -19,14 +32,15 @@ function App() {
         }}
       />
 
-      <AuthDialog
-        show={show}
-        setShow={setShow}
-        setToken={setToken}
-        setRepository={setRepository}
-      />
-
-      <UploadInterface isAuthorized={isAuthorized} />
+      <AuthDialog show={show} setShow={setShow} setToken={setToken} setRepository={setRepository} />
+      <Stack direction={'row'}>
+        <Item>
+          <UploadInterface isAuthorized={isAuthorized} />
+        </Item>
+        <Item>
+          <ProjectForm />
+        </Item>
+      </Stack>
     </>
   );
 }
---
+++ src/ProjectForm.js
@@ -0,0 +1,91 @@
+import React, { useState, useEffect } from 'react';
+import { Card, CardContent, Stack, TextField, Button } from '@mui/material';
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
+import { DesktopDatePicker, LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
+import moment from 'moment';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  color: theme.palette.text.secondary,
+  alignItems: 'center',
+  width: '100%',
+}));
+
+function ProjectForm() {
+  const [name, setName] = useState('');
+  const [projectName, setProjectName] = useState('');
+  const [date, setDate] = useState(moment());
+  const [description, setDescription] = useState('');
+
+  function handleChange(e) {
+    switch (e.target.id) {
+      case 'name':
+        setName(e.target.value);
+
+        break;
+      case 'description':
+        setDescription(e.target.value);
+        break;
+    }
+  }
+
+  function handleDateChange(date) {
+    setDate(date);
+  }
+
+  useEffect(() => {
+    setProjectName(name.replace(/[^A-Za-z_0-9]/g, '-'));
+  }, [name]);
+
+  return (
+    <header className="preview">
+      <Card>
+        <CardContent>
+          <Stack direction="column" justifyContent="center" alignItems="center" spacing={2}>
+            <h2>Project Information</h2>
+            <Item>
+              <TextField
+                id="name"
+                label="Name"
+                inputProps={{ maxLength: 100 }}
+                onChange={handleChange}
+                value={name}
+                helperText={`Your project name will be ${projectName}`}
+                fullWidth
+              />
+            </Item>
+            <Item>
+              <LocalizationProvider dateAdapter={AdapterMoment}>
+                <DesktopDatePicker
+                  label="Date"
+                  inputFormat="MM/DD/YYYY"
+                  value={date}
+                  onChange={handleDateChange}
+                  renderInput={(params) => <TextField {...params} />}
+                />
+              </LocalizationProvider>
+            </Item>
+            <Item>
+              <TextField
+                id="description"
+                label="Description"
+                multiline
+                rows={5}
+                onChange={handleChange}
+                value={description}
+                fullWidth
+              />
+            </Item>
+            <Button variant="contained">Submit</Button>
+          </Stack>
+        </CardContent>
+      </Card>
+    </header>
+  );
+}
+
+export default ProjectForm;
