2789a90f51b6d2c85f8968dddd85e767ba40ba0c
Rudy Chung <61764259+rudychung@users.noreply.github.com>
2022-12-05 15:27:32 -0500

    Add project creation form UI (#28)
    * Add project creation form UI
    
    * Display form contents in console on submit
    
    * Switch from moment to dayjs
    
    * Consolidate material UI imports
    
    * Changed react import statement for consistency
    
    * Minor changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from "react";
-import NavigationBar from "./NavigationBar";
-import AuthDialog from "./AuthDialog";
-import UploadInterface from "./UploadInterface";
+import React, { useState } from 'react';
+import { Stack, Paper, styled } from '@mui/material';
+
+import NavigationBar from './NavigationBar';
+import AuthDialog from './AuthDialog';
+import UploadInterface from './UploadInterface';
+import ProjectForm from './ProjectForm';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  color: theme.palette.text.secondary,
+  minWidth: '50%',
+}));
 
 function App() {
-  const [token, setToken] = React.useState(null);
-  const [repository, setRepository] = React.useState(null);
-  const [show, setShow] = React.useState(true);
+  const [token, setToken] = useState(null);
+  const [repository, setRepository] = useState(null);
+  const [show, setShow] = useState(true);
   const isAuthorized = token && repository;
 
   return (
@@ -19,14 +30,15 @@ function App() {
         }}
       />
 
-      <AuthDialog
-        show={show}
-        setShow={setShow}
-        setToken={setToken}
-        setRepository={setRepository}
-      />
-
-      <UploadInterface isAuthorized={isAuthorized} />
+      <AuthDialog show={show} setShow={setShow} setToken={setToken} setRepository={setRepository} />
+      <Stack direction={'row'}>
+        <Item>
+          <UploadInterface isAuthorized={isAuthorized} />
+        </Item>
+        <Item>
+          <ProjectForm />
+        </Item>
+      </Stack>
     </>
   );
 }
---
+++ src/ProjectForm.js
@@ -0,0 +1,99 @@
+import React, { useState, useEffect } from 'react';
+import { Card, CardContent, Stack, TextField, Button } from '@mui/material';
+import { Paper, styled } from '@mui/material';
+import { DesktopDatePicker, LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import dayjs from 'dayjs';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  color: theme.palette.text.secondary,
+  alignItems: 'center',
+  width: '100%',
+}));
+
+function ProjectForm() {
+  const [name, setName] = useState('');
+  const [projectName, setProjectName] = useState('');
+  const [date, setDate] = useState(dayjs());
+  const [description, setDescription] = useState('');
+
+  function handleChange(e) {
+    switch (e.target.id) {
+      case 'name':
+        setName(e.target.value);
+        break;
+      case 'description':
+        setDescription(e.target.value);
+        break;
+      default:
+        break;
+    }
+  }
+
+  function handleDateChange(date) {
+    setDate(date);
+  }
+
+  function handleSubmit() {
+    console.log(
+      `Name: ${name}\nProjectName: ${projectName}\nDate: ${date}\nDescription${description}`
+    );
+  }
+
+  useEffect(() => {
+    setProjectName(name.replace(/[^A-Za-z_0-9]/g, '-'));
+  }, [name]);
+
+  return (
+    <header className="preview">
+      <Card>
+        <CardContent>
+          <Stack direction="column" justifyContent="center" alignItems="center" spacing={2}>
+            <h2>Project Information</h2>
+            <Item>
+              <TextField
+                id="name"
+                label="Name"
+                inputProps={{ maxLength: 100 }}
+                onChange={handleChange}
+                value={name}
+                helperText={`Your project name will be ${projectName}`}
+                fullWidth
+              />
+            </Item>
+            <Item>
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DesktopDatePicker
+                  label="Date"
+                  inputFormat="YYYY-MM-DD"
+                  value={date}
+                  onChange={handleDateChange}
+                  renderInput={(params) => <TextField {...params} />}
+                />
+              </LocalizationProvider>
+            </Item>
+            <Item>
+              <TextField
+                id="description"
+                label="Description"
+                multiline
+                rows={5}
+                onChange={handleChange}
+                value={description}
+                fullWidth
+              />
+            </Item>
+            <Button variant="contained" onClick={handleSubmit}>
+              Submit
+            </Button>
+          </Stack>
+        </CardContent>
+      </Card>
+    </header>
+  );
+}
+
+export default ProjectForm;
